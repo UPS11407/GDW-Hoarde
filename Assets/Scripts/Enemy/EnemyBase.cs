37a8b36d7f43789a74389ef82e0f875fdf0c1ca3
Lucas Abramczuk <lucas.abramczuk@ontariotechu.net>
2022-11-19 17:12:01 -0500

    Prefabs and enemy base
    I added everything I think will be needed for each enemy
    Created Nav Mesh of level, if layout changes ensure to re-bake the mesh
    Also added a prefab for basic enemy

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyBase : MonoBehaviour
+{
+    public float _maxHP = 4.0f;
+    public float _speed = 2.0f;
+    public float _damage = 1.0f;
+    public float _attackRange = 2.0f;
+    internal float currentHP;
+
+    GameObject player;
+
+    Rigidbody rigid;
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        rigid = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = _speed;
+
+        currentHP = _maxHP;
+    }
+
+    private void Update()
+    {
+        CheckIfDead();
+        ChasePlayer();
+    }
+
+    public void CheckIfDead()
+    {
+        if(currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Returns the distance between the object and the player
+    /// </summary>
+    public float GetPlayerDistance()
+    {
+        return Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    /// <summary>
+    /// Sets the Nav Mesh Agent's target to the player's current position.
+    /// Also stops agent's movement if they are in attack range
+    /// </summary>
+    public void ChasePlayer()
+    {
+        if(GetPlayerDistance() <= _attackRange)
+        {
+            agent.isStopped = true;
+        }
+        else
+        {
+            agent.isStopped = false;
+        }
+
+        if(agent.destination != player.transform.position)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+    }
+
+    public void TakeDamage(float val)
+    {
+        currentHP -= val;
+    }
+}
