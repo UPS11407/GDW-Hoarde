37b590bd831c3aff1ffed3bed26b61b3b36d7fb0
Scott Murphy <scottymurph7@gmail.com>
2023-02-28 20:32:09 -0500

    Inventory work

--- Assets/Scripts/Player/Inventory/Inventory.cs
+++ Assets/Scripts/Player/Inventory/Inventory.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public enum AmmoTypes { STANDARD, EXPLOSIVE, INCINDIARY, SLOW, RAILGUN };
+
     public List<InventorySlot> inventorySlots;
     public TrashSlot trashSlot;
 
+    public int standardAmmo;
+    public int fireAmmo;
+    public int slowAmmo;
+    public int explosiveAmmo;
+    public int railgunCharge;
 
+    public AmmoTypes selectedAmmo;
 
+    public int AmmoCount()
+    {
+        switch (selectedAmmo)
+        {
+            case AmmoTypes.STANDARD: return standardAmmo;
+            case AmmoTypes.EXPLOSIVE: return explosiveAmmo;
+            case AmmoTypes.SLOW: return slowAmmo;
+            case AmmoTypes.INCINDIARY: return fireAmmo;
+            case AmmoTypes.RAILGUN: return railgunCharge;
+        }
+
+        return 0;
+    }
 
     public void AddItem()
     {
--- Assets/Scripts/Player/Inventory/InventoryAttachment.cs
+++ Assets/Scripts/Player/Inventory/InventoryAttachment.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,20 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "InventoryAttachment", menuName = "ScriptableObjects/InventoryAttachment")]
 public class InventoryAttachment : ScriptableObject
 {
-    public enum GunType { PISTOL, RIFLE, RAILGUN };
+    public enum GunType { NULL, PISTOL, RIFLE, RAILGUN };
+    public enum AttachmentType { BARREL, MAGAZINE, GRIP, AMMO };
+    public enum BarrelType { NULL, SNIPER, SHOTGUN, STANDARD, SPEEDCHARGE }
+    public enum AmmoType { NULL, STANDARD, EXPLOSIVE, INCINDIARY, SLOW };
+    public enum MagazineType { NULL, STANDARD, HIGHCAPACITY, QUICKRELOAD, CALICO };
+    public enum GripType { NULL, STANDARD, BURST, AUTO };
 
+    public string attachmentName;
     public Image image;
     public GunType gunType;
     public string description;
+    public AttachmentType attachmentType;
+    public BarrelType barrelType;
+    public MagazineType magazineType;
+    public GripType gripType;
+    public AmmoType ammoType;
 }
