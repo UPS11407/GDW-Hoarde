a258a6b7f3b7afdff895c31468a885ccb7ac37bc
Scott Murphy <scottymurph7@gmail.com>
2023-03-07 16:49:58 -0500

    Drag and drop

---
+++ Assets/Scripts/Player/Inventory/Scripts/DragDrop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    RectTransform trans;
+    Canvas canvas;
+    CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        trans = GetComponent<RectTransform>();
+        canvas = GameObject.Find("UI").GetComponent<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        trans.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+    }
+}
--- Assets/Scripts/Player/Inventory/Scripts/InventorySlot.cs
+++ Assets/Scripts/Player/Inventory/Scripts/InventorySlot.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public InventoryItem item;
     Image image;
     public Sprite emptyImage;
     string attachmentName;
     string attachmentDescription;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            Debug.Log("Drop");
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
 }
