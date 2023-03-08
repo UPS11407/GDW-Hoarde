9c15bb72006e6bd0b986e54979c6ee31a6140c3d
Scott Murphy <scottymurph7@gmail.com>
2023-03-07 20:44:44 -0500

    update

--- Assets/Scripts/Player/Inventory/Scripts/DragDrop.cs
+++ Assets/Scripts/Player/Inventory/Scripts/DragDrop.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     RectTransform trans;
     Canvas canvas;
     CanvasGroup canvasGroup;
     public InventorySlot itemSlot;
+    TrashSlot trashSlot;
+
+    Transform inventoryTransform;
+    RectTransform previousSlot;
 
     private void Awake()
     {
@@ -16,22 +20,26 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
         trans = GetComponent<RectTransform>();
         canvas = GameObject.Find("UI").GetComponent<Canvas>();
         canvasGroup = GetComponent<CanvasGroup>();
+        inventoryTransform = GameObject.Find("Inventory").transform;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (itemSlot != null && !itemSlot.isAmmoSlot)
+        if (itemSlot != null && itemSlot.isAmmoSlot)
         {
             return;
         }
 
+        previousSlot = trans.parent.GetComponent<RectTransform>();
+
+        trans.SetParent(inventoryTransform);
         canvasGroup.blocksRaycasts = false;
         canvasGroup.alpha = 0.6f;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (itemSlot != null && !itemSlot.isAmmoSlot)
+        if (itemSlot != null && itemSlot.isAmmoSlot)
         {
             return;
         }
@@ -41,17 +49,26 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (itemSlot != null && itemSlot.isAmmoSlot)
+        {
+            return;
+        }
+
         canvasGroup.blocksRaycasts = true;
         canvasGroup.alpha = 1f;
+
+        if (transform.parent == inventoryTransform)
+        {
+            trans.anchoredPosition = previousSlot.localPosition;
+            trans.SetParent(previousSlot);
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        
-    }
+        if (itemSlot != null && itemSlot.isAmmoSlot)
+        {
 
-    public void OnDrop(PointerEventData eventData)
-    {
-        
+        }
     }
 }
